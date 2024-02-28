275d7cc4ac319fad8e437e8a857e4ae032193c1e
marogr1168 <marogr1168@W04N-P317D15>
2024-02-28 17:53:56 +0100

    Add project files.

---
+++ Marek_Ogrodnik_lab0/FizzBuzz.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Marek_Ogrodnik_lab0
+{
+    internal class FizzBuzz
+    {
+        private int _maxNumber {  get; set; }
+        public FizzBuzz(int maxNumber) 
+        {
+            _maxNumber = maxNumber;
+        }
+
+        public void Display()
+        {
+            for(int i = 1; i <= _maxNumber; i++) 
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (i % 3 == 0)
+                    Console.WriteLine("Fizz");
+                else if (i % 5 == 0)
+                    Console.WriteLine("Buzz");
+                else
+                    Console.WriteLine(i.ToString());
+            }
+            
+
+
+        }
+    }
+}
---
+++ Marek_Ogrodnik_lab0/Program.cs
@@ -0,0 +1,5 @@
+﻿using Marek_Ogrodnik_lab0;
+
+var fizzBuzz = new FizzBuzz(100);
+
+fizzBuzz.Display();
